320b6ee90ac9f144c9e7af77a0a1348d4b1e7957
Ivan Grgic <ivan.grgic5@gmail.com>
2023-05-18 22:48:46 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <table>
+      <tr>
+        <th>Book name</th>
+        <th>Author</th>
+        <th>ISBN</th>
+        <th>Book cover</th>
+        <th>Borrowed?</th>
+      </tr>
+    </table>
+  )
+}
+
+export default App
